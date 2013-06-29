39f4122cbf713cb6d95a29dab6af0d0c1487450c
liuhui <sheldon.ctc@gmail.com>
2013-06-29 14:50:51 +0800

    UPDATE Makefile and eri_drive.c

--- Makefile
+++ Makefile
@@ -1,30 +1,30 @@
 cc = icc
-OPT = -g -pg -Wall -lm -o
-#OPT = -O3 -fast -lm -o
-#OPT = -O3 -pg -lm -o
+#CFLAGS = -g -pg -Wall -lm -o
+CFLAGS = -O3 -fast -lm -o
+#CFLAGS = -O3 -pg -lm -o
 
 all:  int
 
 parsebasis.o: parsebasis.c parsebasis.h
-	$(cc) parsebasis.c -c $(OPT) parsebasis.o
+	$(cc) parsebasis.c -c $(CFLAGS) parsebasis.o
 
 print.o: print.c print.h parsebasis.h
-	$(cc) print.c -c $(OPT) print.o
+	$(cc) print.c -c $(CFLAGS) print.o
 
 common.o: common.c common.h
-	$(cc) common.c -c $(OPT) common.o
+	$(cc) common.c -c $(CFLAGS) common.o
 
 eri_os.o: eri_os.c eri_os.h common.h print.h
-	$(cc) eri_os.c -c $(OPT) eri_os.o
+	$(cc) eri_os.c -c $(CFLAGS) eri_os.o
 
 eri_hgp.o: eri_hgp.c eri_hgp.h common.h print.h
-	$(cc) eri_hgp.c -c $(OPT) eri_hgp.o
+	$(cc) eri_hgp.c -c $(CFLAGS) eri_hgp.o
 
 eri_drive.o: eri_drive.c eri_drive.h common.h
-	$(cc) eri_drive.c -c $(OPT) eri_drive.o
+	$(cc) eri_drive.c -c $(CFLAGS) eri_drive.o
 
 int: int.c print.o common.o parsebasis.o eri_drive.o eri_os.o eri_hgp.o
-	$(cc) int.c print.o common.o parsebasis.o eri_drive.o eri_os.o eri_hgp.o $(OPT) int
+	$(cc) int.c print.o common.o parsebasis.o eri_drive.o eri_os.o eri_hgp.o $(CFLAGS) int
 
 clean:
 	rm -rf int *.o gmon.out
--- eri_drive.c
+++ eri_drive.c
@@ -43,31 +43,32 @@ double**** ERI_Matrix(INPUT_INFO* b)
         for (j = 0; j < basis_count; j++) {
             for (k = 0; k < basis_count; k++) {
                 for (l = 0; l < basis_count; l++) {
-                    debug = 0;
-                    if (i == 0 && j == 0 && k == 13 && l == 4) {
-                        debug = 90;
-                        fprintf(stdout, "---%d---%d---%d---%d---\n", i, j, k, l);
-                    }
+                    //debug = 0;
+                    //if (i == 0 && j == 0 && k == 13 && l == 4) {
+                    //    debug = 90;
+                    //    fprintf(stdout, "---%d---%d---%d---%d---\n", i, j, k, l);
+                    //}
 
-                    // ChkERISym(e2, i, j, k, l, basis_count, &is_dup);
-                    // if (is_dup)     continue;
+                    ChkERISym(e2, i, j, k, l, basis_count, &is_dup);
+                    if (is_dup)     continue;
 #if DEBUG_ERI_BASIS_NO
             fprintf(stdout, "---%d---%d---%d---%d---\n", i, j, k, l);
 #endif
-                    // e2[i][j][k][l] = \
-                    // e2[i][j][l][k] = \
-                    // e2[j][i][k][l] = \
-                    // e2[j][i][l][k] = \
-                    // e2[k][l][i][j] = \
-                    // e2[k][l][j][i] = \
-                    // e2[l][k][i][j] = \
-                    // e2[l][k][j][i] = ERI_basis_OS(
-                    //                             &basisSet[i],
-                    //                             &basisSet[j],
-                    //                             &basisSet[k],
-                    //                             &basisSet[l], b, debug);
+                    e2[i][j][k][l] = \
+                    e2[i][j][l][k] = \
+                    e2[j][i][k][l] = \
+                    e2[j][i][l][k] = \
+                    e2[k][l][i][j] = \
+                    e2[k][l][j][i] = \
+                    e2[l][k][i][j] = \
+                    //e2[l][k][j][i] = ERI_HGP_DRIVE(b, i, j, k, l, debug);
+                    e2[l][k][j][i] = ERI_basis_OS(
+                                                &basisSet[i],
+                                                &basisSet[j],
+                                                &basisSet[k],
+                                                &basisSet[l], b, debug);
                     //e2[i][j][k][l] = ERI_basis_OS( &basisSet[i], &basisSet[j], &basisSet[k], &basisSet[l], b, debug);
-                    e2[i][j][k][l] = ERI_HGP_DRIVE(b, i, j, k, l, debug);
+                    //e2[i][j][k][l] = ERI_HGP_DRIVE(b, i, j, k, l, debug);
                 }
             }
         }
